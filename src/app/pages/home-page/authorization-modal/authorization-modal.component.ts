c6a6bd3e1f0e90e96de1424247ba560af01c866b
Denis Derkach <podstavka128@gmail.com>
2019-05-16 13:47:45 +0300

    feature/authorization, create authorization modal component

---
+++ src/app/pages/home-page/authorization-modal/authorization-modal.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-authorization-component',
+    templateUrl: './authorization-modal.component.html',
+    styleUrls: ['./authorization-modal.component.scss']
+})
+export class AuthorizationModalComponent {}
+
