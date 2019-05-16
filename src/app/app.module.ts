0a9a2cc19920f8969c5db78fa895b817242192ea
Denis Derkach <podstavka128@gmail.com>
2019-05-16 13:44:01 +0300

    feature/app, improve app module, component and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { HomePageModule } from './pages/home-page/home-page.module';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { MaterialSharedModule } from './shared/material-shared.module';
+import { SharedModule } from './shared/shared.module';
+import { FirebaseSharedModule } from './shared/firebase-shared.module';
+import { FirebaseService } from './core/services/firebase.services';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    SharedModule,
+    AppRoutingModule,
+    HomePageModule,
+    MaterialSharedModule,
+    FirebaseSharedModule
   ],
   providers: [],
+  exports: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
