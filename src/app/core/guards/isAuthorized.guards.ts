7e71ba7d781b8ce1a462c6be9473ad726be12012
Denis Derkach <podstavka128@gmail.com>
2019-05-28 22:30:37 +0300

    feature/guards, add guards for non authorized users

---
+++ src/app/core/guards/isAuthorized.guards.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Route, Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { FirebaseService } from '../services/firebase.services';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class CanLoadApplicationPage implements CanLoad {
+    constructor(private router: Router, private fireAuthService: FirebaseService) { }
+
+    canLoad(route: Route): Observable<boolean> {
+        return this.fireAuthService.isSignedIn()
+            .pipe(
+                map(isUser => {
+                    console.warn('CanLoadApplicationPage', isUser);
+                    if (isUser) {
+                        return true;
+                    }
+
+                    this.router.navigate(['/auth']);
+                    return false;
+                })
+            );
+    }
+}
+
+@Injectable()
+export class CanLoadAuthPage implements CanLoad {
+    constructor(private router: Router, private fireAuthService: FirebaseService) { }
+
+    canLoad(route: Route): Observable<boolean> {
+        return this.fireAuthService.isSignedIn()
+            .pipe(
+                map(isUser => {
+                    if (!isUser) {
+                        return true;
+                    }
+
+                    this.router.navigate(['/home']);
+                    return false;
+                })
+            );
+    }
+}
+
+@Injectable()
+export class CanActivateApplicationPage implements CanActivate {
+    constructor(private router: Router, private fireAuthService: FirebaseService) { }
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean> {
+        return this.fireAuthService.isSignedIn()
+        .pipe(
+            map(isUser => {
+                console.warn('CanActivateApplicationPage', isUser);
+                if (isUser) {
+                    return true;
+                }
+
+                this.router.navigate(['/auth']);
+                return false;
+            })
+        );
+    }
+}
