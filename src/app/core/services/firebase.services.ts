847a1775c24ec022beb297090957685901598673
Denis Derkach <podstavka128@gmail.com>
2019-05-28 22:31:52 +0300

    feature/firebase, improve firebase service

--- src/app/core/services/firebase.services.ts
+++ src/app/core/services/firebase.services.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { User } from 'firebase';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable()
 export class FirebaseService {
-    constructor(private fireAuth: AngularFireAuth) { }
+    constructor(private fireAuth: AngularFireAuth, private firestore: AngularFirestore) {
+        this.fireAuth.authState.subscribe(user => {
+            this.currentUser = user;
+        });
+    }
+
+    private currentUser;
 
     signIn({ email, password }) {
         return this.fireAuth.auth.signInWithEmailAndPassword(email, password);
@@ -13,15 +22,23 @@ export class FirebaseService {
         return this.fireAuth.auth.createUserWithEmailAndPassword(email, password);
     }
 
-    signOut() {
-        this.fireAuth.auth.signOut();
+    signOut(): Promise<void> {
+        return this.fireAuth.auth.signOut();
     }
 
-    getCurrentUser() {
-        return this.fireAuth.auth.currentUser;
+    getCurrentUser(): User {
+        return this.currentUser;
     }
 
-    isUserSignedIn() {
-        return !!this.fireAuth.auth.currentUser;
+    isCurrentUser(): boolean {
+        return !!this.currentUser;
+    }
+
+    isSignedIn(): Observable<User> {
+        return this.fireAuth.authState;
+    }
+
+    registerUserInFirestore({ uid, email }) {
+        this.firestore.collection('users').doc(uid).set({uid, email});
     }
 }
