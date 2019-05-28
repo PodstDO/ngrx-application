b0487316411eb2f0818f9eba826b7c06ecf638e4
Denis Derkach <podstavka128@gmail.com>
2019-05-28 22:38:28 +0300

    feature/header, improve component

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { FirebaseService } from 'src/app/core/services/firebase.services';
 
 @Component({
     selector: 'app-header',
@@ -7,11 +8,23 @@ import { Router } from '@angular/router';
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-    constructor(public router: Router) {}
+    constructor(
+        public router: Router,
+        private firebaseService: FirebaseService
+    ) { }
 
     @Output() sidebarToggle = new EventEmitter();
+    @Output() sidebarClose = new EventEmitter();
+
+    onSidebarClose(): void {
+        this.sidebarClose.emit();
+    }
 
     toggleSidebar(): void {
         this.sidebarToggle.emit();
+    }
+
+    isCurrentUser(): boolean {
+        return this.firebaseService.isCurrentUser();
     }
 }
